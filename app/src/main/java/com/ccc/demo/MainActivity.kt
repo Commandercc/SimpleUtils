3a802b63bd645f5a3d55f625264de3d7e3df7bb7
Commandercc <1502149172@qq.com>
2020-12-19 18:53:22 +0800

    update new pack

---
+++ app/src/main/java/com/ccc/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ccc.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ccc.simpleutils.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
